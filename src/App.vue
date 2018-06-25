f5b9b8ae141a5a4360ca308c4a423c56481f3a82
Koltz, Marty <mkoltz2@gmail.com>
2018-06-25 12:17:06 -0600

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,122 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id = "app">
+  <div class = "header">
+          <img src="img/F.png">
+          <h1>You're Ultimate Brew Guide</h1>
+          <p>These guides serve as a good place to start.<br>
+          Start with learning how the espresso machine works.</p>
+
+      </div> 
+
+     <brewguide-card
+     v-for="item in card_list"
+     v-bind:card="item"
+     v-bind:key="item.id">       
+     </brewguide-card>
+</div>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import card_list from './data/cards.js';
+import brewguideCard from './components/brewguide-card.vue';
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    brewguideCard
+  },
+  data:function () {
+    return {
+      card_list
+    }
   }
 }
 </script>
 
+
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  display: flex;
+  font-family: "Open Sans";
+  align-content: flex-start;
+  flex-wrap: wrap;
+
+}
+
+.brewguide_card{
+  background: linear-gradient(to bottom, #f1f5f4 0%,#f1f5f4 50%,#000000 50%,white 50%,white 100%); /* W3C */
+  display: flex;
+    flex-direction: column;
+    width: 315px;
+    height: 500px;
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.50);
+    align-items: center;
+    margin:10px;
+}
+
+.card_image{
+
+}
+
+.header{
+     width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 80px;
+    border: 2px solid #A0945A;
+}
+
+.header img{
+    position: relative;
+  top: -20px;
+}
+
+.header h1{
+  font-size: 40px;
+ text-transform: uppercase;
+ margin: 15px 0 0 0;
+}
+
+.header p{
+    color: #A0945A;
+    font-style: italic;
+    font-size: 18px;
+    padding: 0 70px;
+    text-align: center;
+    font-weight: 300;
+}
+
+
+.brewguide_card_title{
+font-family: "Open Sans";
+font-size: 22px;
+color: #000000;
+letter-spacing: 0;
+text-align: center;
+text-transform: uppercase;
+}
+
+.pronounce{
+  /* cap·puc·ci·no: */
+font-family: "Open Sans";
+font-style: italic;
+font-size: 30px;
+color: #A0945A;
+letter-spacing: 0;
+text-align: center;
+line-height: 32px;
+}
+
+.brewguide_card hr{
+        width:40%;
+      border: 0;
+    height: 4px;
+    background: #A0945A;
 }
 </style>
