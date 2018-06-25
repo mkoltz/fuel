03e77632e34c2ab0279c6f710b543355b04a6605
Koltz, Marty <mkoltz2@gmail.com>
2018-06-25 12:17:35 -0600

    no message

---
+++ src/components/brewguide-card.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <div class="brewguide_card"> 
+  <img class="card_image" :src="card.image"></img>
+  <hr>
+  <p class="brewguide_card_title">{{card.title}}</p>
+  <p class="pronounce">{{card.pronounce}}</p></div>
+  
+
+</template>
+
+
+
+<script>
+
+
+export default {
+  name: 'brewguide-card',
+  props:['card']
+}
+
+</script>
+
+
+
+
+<style>
+
+</style>
---
+++ src/data/cards.js
@@ -0,0 +1,35 @@
+
+const cards = [
+  {
+    "id":"0",
+    "image":"img/machine.png",
+    "title":"machine 101",
+    "pronounce":" "
+  },
+  {
+    "id":"1", 
+    "image":"img/americano.png",
+    "title":"americano",
+    "pronounce":"es * pres * so"
+  },
+  {
+    "id":"2", 
+    "image":"img/flat_white.png",
+    "title":"flat white",
+    "pronounce":"es * pres * so"
+  },
+  {
+    "id":"3", 
+    "image":"img/cappuccino.png",
+    "title":"cappaccino",
+    "pronounce":"es * pres * so"
+  },
+  {
+    "id":"4", 
+    "image":"img/espresso.png",
+    "title":"ESPRESSO",
+    "pronounce":"es * pres * so"
+  }
+];
+
+export default cards;
